94bd2f2ef5aa5df6e4182db9ea2fb9822e38a1ea
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2023-11-24 11:59:59 +0100

    report page flow

--- src/app/dashboard/report/page.tsx
+++ src/app/dashboard/report/page.tsx
@@ -1,8 +1,23 @@
+import StyledTabs from '@/common/StyledTabs';
 import DashboardLayout from '@/components/layout/Dashboard/DashboardLayout';
+import UserPerformance from '@/components/report/UserPerformance';
+import UserReport from '@/components/report/UserReport';
+import UserTestLog from '@/components/report/UserTestLog';
 import React from 'react';
 
+const tabs = ['Performance', 'Report', 'Test Log'];
+const panels = [
+  <UserPerformance key='Performance' />,
+  <UserReport key='Report' />,
+  <UserTestLog key='Test Log' />,
+];
+
 const ReportPage = () => {
-  return <DashboardLayout pageTitle='Report & Analytics'>ReportPage</DashboardLayout>;
+  return (
+    <DashboardLayout pageTitle='Report & Analytics'>
+      <StyledTabs tabs={tabs} panels={panels} />
+    </DashboardLayout>
+  );
 };
 
 export default ReportPage;
