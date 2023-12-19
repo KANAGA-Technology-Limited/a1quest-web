a7ea21b792d406bb99c93de522df60b6ec8655c3
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2023-12-19 23:33:43 +0100

    recent learning fix

--- src/components/learning/class/RecentLearning.tsx
+++ src/components/learning/class/RecentLearning.tsx
@@ -5,10 +5,13 @@ import { sendCatchFeedback } from '@/functions/feedback';
 import { RecentTopicType } from '@/types/data';
 import React, { useEffect, useState } from 'react';
 import RecentTopicCard from './RecentTopicCard';
+import { useSearchParams } from 'next/navigation';
 
 const RecentLearning = () => {
   const [topics, setTopics] = useState<RecentTopicType[] | undefined>(undefined);
   const [loading, setLoading] = React.useState(true);
+  const searchParams = useSearchParams();
+  const classId = searchParams.get('id');
 
   useEffect(() => {
     const getTopics = async () => {
@@ -41,9 +44,13 @@ const RecentLearning = () => {
 
   return (
     <div className='mb-10 flex flex-col w-full gap-6'>
-      {topics.slice(0, 2).map((topic) => (
-        <RecentTopicCard key={topic._id} topic={topic} />
-      ))}
+      {topics.slice(0, 2).map(
+        // Show only topics in that class
+        (topic) =>
+          topic.topic_id?.class_id === classId && (
+            <RecentTopicCard key={topic._id} topic={topic} />
+          )
+      )}
     </div>
   );
 };
