a2f764207acca37f50f544324e978d3f862cd8a8
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2023-12-22 02:04:10 +0100

    vertical chart fix

--- src/components/report/UserPerformance/GeneralPerformance.tsx
+++ src/components/report/UserPerformance/GeneralPerformance.tsx
@@ -16,18 +16,40 @@ import {
 const GeneralPerformance = ({ data }: { data: TopicPerformanceType }) => {
   return (
     <div className='w-full'>
-      <ResponsiveContainer width='100%' height={300}>
-        <BarChart width={500} height={300} data={data.tests}>
+      <ResponsiveContainer width='100%' height={800}>
+        <BarChart
+          width={500}
+          height={800}
+          data={data.tests.slice(0, 10)}
+          layout='vertical'
+          margin={{
+            left: 30,
+          }}
+          barCategoryGap={14}
+        >
           <CartesianGrid strokeDasharray='3 3' />
-          <XAxis dataKey='sub_topic' axisLine={false} tickLine={false} />
-          <YAxis axisLine={false} tickLine={false} />
+          <XAxis
+            axisLine={false}
+            tickLine={false}
+            dataKey='percentage'
+            type='number'
+            tickFormatter={(label) => (label ? `${label}%` : label)}
+            domain={[0, 100]}
+          />
+          <YAxis
+            axisLine={false}
+            tickLine={false}
+            dataKey='sub_topic'
+            type='category'
+            // minTickGap={20}
+          />
           <Tooltip />
           <Bar
             dataKey='percentage'
             fill='#059669'
             activeBar={<Rectangle fill='#F0AC27' />}
             barSize={15}
-            radius={[4, 4, 0, 0]}
+            radius={[0, 4, 4, 0]}
           />
         </BarChart>
       </ResponsiveContainer>
